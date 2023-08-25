e75866aa6348c027e540e6c556b575a0b2d595dc
akshatk806 <akshatk806@gmail.com>
2023-08-25 10:07:55 +0530

    2. creating firebase config file on frontend to connect with firebase on backend

---
+++ src/firebase/config.js
@@ -0,0 +1,26 @@
+import firebase from "firebase/app";
+
+// importing services
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA8aakvE-_a26vSJg647NVAi9NYpGYEzsk",
+    authDomain: "myproject-site.firebaseapp.com",
+    projectId: "myproject-site",
+    storageBucket: "myproject-site.appspot.com",
+    messagingSenderId: "116746080311",
+    appId: "1:116746080311:web:d311e2a0ef462e372a2fb2"
+};
+
+// initilize firebase
+firebase.initializeApp(firebaseConfig);
+
+// initilize services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// firebase timestamp
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
