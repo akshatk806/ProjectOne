0b5b2481c7aeea1f56afcd4b16ce6d3a76b718b2
akshatk806 <akshatk806@gmail.com>
2023-08-27 20:53:47 +0530

    8. creating a signup form in signup component

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,11 +1,56 @@
+import { useState } from 'react'
+
 // styles
 import './Signup.css'
 
 const Signup = () => {
+  // states for the field
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [thumbnail, setThumbnail] = useState(null);     // nothing selected to begin with
+
+
   return (
-    <div>
-        Signup
-    </div>
+    <form className="auth-form">
+        <h2>Sign up</h2>
+        <label>
+          <span>email:</span>
+          <input 
+            type="email" 
+            required
+            onChange={e => setEmail(e.target.value)}
+            value={email}
+          />
+        </label>
+        <label>
+          <span>password:</span>
+          <input 
+            type="password" 
+            required
+            onChange={e => setPassword(e.target.value)}
+            value={password}
+          />
+        </label>
+        <label>
+          <span>name:</span>
+          <input 
+            type="text" 
+            required
+            onChange={e => setDisplayName(e.target.value)}
+            value={displayName}
+          />
+        </label>
+
+        {/* for uploading image */}
+        <label>
+          <span>profile thumbnail:</span>
+          <input 
+            type="file"
+          />
+        </label>
+        <button className="btn">Sign Up</button>
+    </form>
   )
 }
 
