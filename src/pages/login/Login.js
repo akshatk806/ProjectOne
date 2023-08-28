fb0aaaa89edb4eeb5fe2303bcd1ff2674b23e5de
akshatk806 <akshatk806@gmail.com>
2023-08-28 20:32:51 +0530

    15. making the login form

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,11 +1,44 @@
+import { useState } from 'react';
+
 // styles
-import './Login.css'
+import './Login.css'   // auth-form class css written in signup.css but that signup.css is become global so we don't need to write another css code in login.css
+// the signup.css is global file and its injected in head of the page unlike css modules 
 
 const Login = () => {
+  // state for the fields
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(email, password)
+  }
+
   return (
-    <div>
-        Login
-    </div>
+    <form className="auth-form" onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label>
+        <span>email:</span>
+        <input 
+          type="email" 
+          required
+          onChange={e => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input 
+          type="password" 
+          required
+          onChange={e => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      {!isPending && <button className="btn">Login</button>}
+      {isPending && <button className="btn" disabled>loading...</button>}
+      {error && <div className="error">{error}</div>}
+    </form>
   )
 }
 
