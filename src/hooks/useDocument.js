7265debb62ce89c1a4b07248960ff45b93785d22
akshatk806 <akshatk806@gmail.com>
2023-09-04 00:46:03 +0530

    30. making a useDocument hook

---
+++ src/hooks/useDocument.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+import { projectFirestore } from "../firebase/config"
+
+export const useDocument = (collection, id) => {
+    const [document, setDocument] = useState(null)
+    const [error, setError] = useState(null)
+
+    // realtime data for the document
+    useEffect(()=> {
+        // ref to that particular document
+        const ref = projectFirestore.collection(collection).doc(id)
+
+        const unsubsribe = ref.onSnapshot(snapshot => {               // .onSnapshot method returns and an unsubsribe funtion and now we have to invoke that function in line 22 
+            setDocument({ ...snapshot.data(), id: snapshot.id })
+            setError(null);
+        }, (err) => {
+            console.log(err.message)
+            setError('failed to get document')
+        })
+
+        // cleanup function so that we unsubscribe the line 13 if we leave up the page. Return a function which fires up whenever the component that uses this useDocument hook unmounts, so if we go another page component unmounts and fires cleanup function at that we have to unsubsribe from real time data of line 11 for the document
+        return () => unsubsribe()
+
+    }, [collection, id])
+
+    return { document, error }
+}
