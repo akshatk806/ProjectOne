79fcfc10545b14cc226f2cac37a82cac8778f61d
akshatk806 <akshatk806@gmail.com>
2023-09-05 00:46:38 +0530

    36. listing the comments

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -3,6 +3,8 @@ import { timestamp } from "../../firebase/config"
 import { useAuthContext } from "../../hooks/useAuthContext"
 import { useFirestore } from "../../hooks/useFirestore"
 
+import Avatar from "../../components/Avatar"
+
 const ProjectComments = ({ project }) => {
     const { updateDocument, response } = useFirestore('projects');
     const [newComment, setNewComment] = useState('');
@@ -33,6 +35,25 @@ const ProjectComments = ({ project }) => {
   return (
     <div className="project-comments">
         <h4>Project Comments</h4>
+
+        {/* listing all the comments */}
+        <ul>
+            {project.comments.length > 0 && project.comments.map(comment => (
+                <li key={comment.id}>
+                    <div className="comment-author">
+                        <Avatar src={comment.photoURL}/>
+                        <p>{comment.displayName}</p>
+                    </div>
+                    <div className="comment-date">
+                        <p>date here</p>
+                    </div>
+                    <div className="comment-content">
+                        <p>{comment.content}</p>
+                    </div>
+                </li>
+            ))}
+        </ul>
+
         <form className="add-comment" onSubmit={handleSubmit}>
             <label>
                 <span>Add New Comment:</span>
